898ec9364611d94f53a67ca6f8e453df02810164
Liu Zhaonan <3@xinyueshikong.com>
2017-05-26 08:57:35 +0000

    战斗首次提交

---
+++ trunk/Client/Assets/Scripts/Logic/BattleSceneTouch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSceneTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10000f))
+            {
+                if (hit.transform.CompareTag("Point"))
+                {
+                    PointHandle handler = hit.transform.GetComponent<PointHandle>();
+                    if (handler != null)
+                        handler.Excute();
+                }
+            }
+        }
+	}
+}
