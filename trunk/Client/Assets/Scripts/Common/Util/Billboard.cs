9a07a26e01664369dffedfa582821da2d85e933a
Liu Zhaonan <3@xinyueshikong.com>
2017-07-26 08:22:19 +0000

    客户端逻辑

---
+++ trunk/Client/Assets/Scripts/Common/Util/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using FairyGUI;
+
+public class Billboard : MonoBehaviour {
+
+    UIPanel panel;
+	// Use this for initialization
+	void Start () {
+        //panel = GetComponent<UIPanel>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(Vector3.up, 180f);
+	}
+}
