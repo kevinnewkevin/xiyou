c51a5e78530f9bd7c4eccd03557484f036d20962
Guo Yanlin <hotlala8088@outlook.com>
2017-08-02 03:33:15 +0000

    add CC SERVER

--- trunk/tools/rpc.bat
+++ trunk/tools/rpc.bat
@@ -1,3 +1,4 @@
 rpc.exe -i ../schema/protocol.rpc -o ../Client/Assets/Scripts/Common/Rpc/ -g cs
 rpc.exe -i ../schema/protocol.rpc -o ../server/src/logic/ -g go
+rpc.exe -i ../schema/protocol.rpc -o ../server/prpc_gen/ -g cc
 pause
