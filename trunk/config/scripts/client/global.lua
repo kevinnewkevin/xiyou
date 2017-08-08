7a4f53424b418d58bfbb1b6779ed8838eaacf777
Liu Zhaonan <3@xinyueshikong.com>
2017-08-08 03:11:36 +0000

    客户端逻辑

---
+++ trunk/config/scripts/client/global.lua
@@ -0,0 +1,21 @@
+
+--主入口函数。从这里开始lua逻辑
+function Main()					
+	 		
+end
+
+--场景切换通知
+function OnLevelWasLoaded(level)
+	collectgarbage("collect")
+	Time.timeSinceLevelLoad = 0
+end
+
+function ExcuteNpc(id)
+	if id == 1 then
+		Proxy4Lua.BattleJoin();
+	end
+end
+
+function RegGlobalValue()
+	Define.Set("UIModelScale", 200);
+end
