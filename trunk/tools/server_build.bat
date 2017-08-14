af0e5e5ce77106e0052131ae0103e97e27f1aad1
Guo Yanlin <hotlala8088@outlook.com>
2017-08-14 12:32:05 +0000

    添加服务器环境

--- trunk/tools/server_build.bat
+++ trunk/tools/server_build.bat
@@ -2,7 +2,7 @@ SET GOROOT=%cd%\golang
 
 SET GOPATH=%cd%\golang\external;%cd%\..\server
 
-SET PATH=%PATH%;%cd%\golang\bin
+SET PATH=%PATH%;%cd%\golang\bin;%cd%\gcc64\bin
 
 cd ..\server\src\logic
 
--- trunk/tools/server_env.bat
+++ trunk/tools/server_env.bat
@@ -1,9 +1,11 @@
 
-if exist golang.zip echo "has golang.zip " else wget http://10.10.10.252/ftp/develop-kits/golang.zip
+wget http://10.10.10.252/ftp/develop-kits/golang.zip
 
 7za x golang.zip
 
+wget http://10.10.10.252/ftp/develop-kits/gcc64.zip
 
+7za x gcc64.zip
 
 
 
