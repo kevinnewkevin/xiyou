d7df77fc685d7b333e93666ac70f3e9b9387b3fb
Liu Zhaonan <3@xinyueshikong.com>
2017-08-25 06:14:35 +0000

    客户端逻辑

--- trunk/Client/Assets/Scripts/Common/Util/Billboard.cs
+++ trunk/Client/Assets/Scripts/Common/Util/Billboard.cs
@@ -4,17 +4,18 @@ using FairyGUI;
 
 public class Billboard : MonoBehaviour {
 
-    UIPanel panel;
+
+    Camera _MainCam;
 	// Use this for initialization
 	void Start () {
-        //panel = GetComponent<UIPanel>();
+        _MainCam = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//        transform.LookAt(Camera.main.transform);
-//        transform.Rotate(Vector3.up, 180f);
-        transform.LookAt(Camera.main.transform.position, -Vector3.up);
-        transform.Rotate(Vector3.left, 180f);
+//        transform.LookAt(Camera.main.transform.position, -Vector3.up);
+//        transform.Rotate(Vector3.left, 180f);
+
+        transform.rotation = _MainCam.transform.rotation;
 	}
 }
