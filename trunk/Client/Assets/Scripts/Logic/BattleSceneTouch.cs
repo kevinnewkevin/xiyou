9d4401e0702c48554a99cedd562d5b179bcd5874
Liu Zhaonan <3@xinyueshikong.com>
2017-08-28 06:59:59 +0000

    客户端逻辑

--- trunk/Client/Assets/Scripts/Logic/BattleSceneTouch.cs
+++ trunk/Client/Assets/Scripts/Logic/BattleSceneTouch.cs
@@ -5,13 +5,20 @@ using FairyGUI;
 
 public class BattleSceneTouch : MonoBehaviour {
 
+    bool _IsPress;
+
+    float _Premag;
+
 	// Use this for initialization
 	void Start () {
         Stage.inst.onTouchBegin.Add(OnTouchBegin);
+        Stage.inst.onTouchMove.Add(OnTouchMove);
+        Stage.inst.onTouchEnd.Add(OnTouchEnd);
 	}
 
     void OnTouchBegin()
     {
+        _IsPress = true;
         if (!Stage.isTouchOnUI)
         {
             RaycastHit hit;
@@ -25,6 +32,32 @@ public class BattleSceneTouch : MonoBehaviour {
                         handler.Excute();
                 }
             }
+        }
+    }
+
+    void OnTouchMove(EventContext context)
+    {
+        if (!Stage.isTouchOnUI)
+        {
+            if (_IsPress)
+            {
+                float crtmag = ((InputEvent)context.data).position.magnitude;
+                bool anti = false;
+                if (_Premag > crtmag)
+                    anti = true;
+                _Premag = crtmag;
+                Camera.main.transform.RotateAround(Battle._Center, Vector3.up, crtmag * Time.deltaTime * 0.1f * (anti? -1f: 1f));
+                Camera.main.transform.LookAt(Battle._Center);
+            }
+        }
+    }
+
+    void OnTouchEnd()
+    {
+        _IsPress = false;
+        if (!Stage.isTouchOnUI)
+        {
+
         }
     }
 
--- trunk/Client/Assets/Scripts/Logic/MainSceneTouch.cs
+++ trunk/Client/Assets/Scripts/Logic/MainSceneTouch.cs
@@ -5,52 +5,35 @@ using FairyGUI;
 
 public class MainSceneTouch : MonoBehaviour {
 
-    bool isMove;
-    bool isPress;
-    float preX;
+    bool _IsPress;
+    bool _IsMoved;
 
-    enum slideVector { nullVector, left, right };  
-
-    private Vector2 lastPos;//上一个位置  
-
-    private Vector2 currentPos;//下一个位置  
-
-    private slideVector lastVector = slideVector.nullVector;//当前滑动方向  
-
-    private slideVector currentVector = slideVector.nullVector;//当前滑动方向  
-
-    private float timer;//时间计数器  
-
-    public float offsetTime = 0.01f;//判断的时间间隔  
-
-    bool isMoveScreen;
+    float _Premag;
 
 	// Use this for initialization
 	void Start () {
         World.InitPlayerActor();
         World.InitNpcActor();
-//        Stage.inst.onTouchBegin.Add(OnTouchBegin);
-//        Stage.inst.onTouchEnd.Add(OnTouchEnd);
-//        Stage.inst.onTouchMove.Add(OnTouchMove);
+        Stage.inst.onTouchBegin.Add(OnTouchBegin);
+        Stage.inst.onTouchEnd.Add(OnTouchEnd);
+        Stage.inst.onTouchMove.Add(OnTouchMove);
 	}
 
     void OnTouchBegin(EventContext context)
     {
+        _Premag = ((InputEvent)context.data).position.magnitude;
+        _IsPress = true;
+        _IsMoved = false;
         if (Stage.isTouchOnUI)
             return;
-        
-        isPress = true;
-        preX = ((FairyGUI.InputEvent)context.data).x;
     }
 
     void OnTouchEnd()
     {
-        isPress = false;
-        if (isMove)
-        {
-            isMove = false;
+        _IsPress = false;
+        if (_IsMoved)
             return;
-        }
+
         if (!Stage.isTouchOnUI)
         {
             RaycastHit hit;
@@ -77,81 +60,16 @@ public class MainSceneTouch : MonoBehaviour {
 
     void OnTouchMove(EventContext context)
     {
-        if (Stage.isTouchOnUI)
+        if (!Stage.isTouchOnUI)
         {
-            isPress = false;
-            isMove = false;
-            return;
-        }
-
-        if (!isPress)
-            return;
-        
-        float dir = ((FairyGUI.InputEvent)context.data).x - preX;
-        Camera.main.GetComponent<CameraTracker>().MoveToLookAt += dir;
-        preX = ((FairyGUI.InputEvent)context.data).x;
-        isMove = true;
-        Debug.Log(dir);
-    }
-
-    void Update()
-    {
-        if (Stage.isTouchOnUI)
-            return;
-        
-        if (Input.GetMouseButtonDown (0))
-        {  
-            lastPos = Input.mousePosition;  
-            currentPos = Input.mousePosition;  
-            timer = 0;  
-        }
-        if (Input.GetMouseButton (0))
-        {  
-            currentPos = Input.mousePosition;  
-            if (currentPos.x < lastPos.x)
+            if (_IsPress)
             {
-                Camera.main.GetComponent<CameraTracker>().MoveToLookAt -= 1f;
-                isMoveScreen = true;
-            }   
-            if (currentPos.x > lastPos.x)
-            {
-                Camera.main.GetComponent<CameraTracker>().MoveToLookAt += 1f;
-                isMoveScreen = true;
+                float crtmag = ((InputEvent)context.data).position.magnitude;
+                Camera.main.transform.Translate(new Vector3(_Premag - crtmag, 0f, 0f) * Time.deltaTime * 0.5f);
+                _Premag = crtmag;
+                _IsMoved = true;
             }
-
-            lastPos = currentPos;  
         }
-        if (Input.GetMouseButtonUp (0))
-        { 
-            if (isMoveScreen)
-            {
-                isMoveScreen = false;
-                return;
-            }
-            if (!Stage.isTouchOnUI)
-            {
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(new Vector2(Stage.inst.touchPosition.x, Screen.height - Stage.inst.touchPosition.y));
-                if (Physics.Raycast(ray, out hit))
-                {
-                    if (hit.transform.name.Equals("Cube"))
-                    {
-                        Camera.main.GetComponent<CameraTracker>().MoveToLookAt = hit.point.x;
-                        World.PlayerActor.MoveTo(new Vector3(hit.point.x, -14.31f, hit.point.z), delegate {
-                            World.PlayerActor.Stop();
-                        });
-                    }
-
-                    if (hit.transform.CompareTag("Npc"))
-                    {
-                        NpcHandle handler = hit.transform.GetComponent<NpcHandle>();
-                        if (handler != null)
-                            handler.Excute();
-                    }
-                }
-            }
-            currentVector = slideVector.nullVector;  
-        }  
     }
 
     void OnDestroy()
@@ -160,13 +78,4 @@ public class MainSceneTouch : MonoBehaviour {
         Stage.inst.onTouchEnd.Remove(OnTouchEnd);
         Stage.inst.onTouchMove.Remove(OnTouchMove);
     }
-
-//    void OnGUI()
-//    {
-//        if (GUILayout.Button("加buff"))
-//            World.PlayerActor.AddBuff(1);
-//
-//        if (GUILayout.Button("减buff"))
-//            World.PlayerActor.RemoveBuff(1);
-//    }
 }
