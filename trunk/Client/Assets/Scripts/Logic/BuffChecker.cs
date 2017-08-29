d5b5a9d90f7fa18bfb89f197cb049813d39bba8b
Liu Zhaonan <3@xinyueshikong.com>
2017-08-29 02:54:33 +0000

    客户端逻辑

--- trunk/Client/Assets/Scripts/Logic/BuffChecker.cs
+++ trunk/Client/Assets/Scripts/Logic/BuffChecker.cs
@@ -41,8 +41,11 @@ public class BuffChecker {
 
         new Timer().Start(new TimerParam(data._AnimTime, delegate
         {
-            _Actor.Play(Define.ANIMATION_PLAYER_ACTION_BEATTACK);
-            _Actor.PlayQueue(Define.ANIMATION_PLAYER_ACTION_IDLE);
+            if(_BuffCheck[0].BuffData < 0)
+            {
+                _Actor.Play(Define.ANIMATION_PLAYER_ACTION_BEATTACK);
+                _Actor.PlayQueue(Define.ANIMATION_PLAYER_ACTION_IDLE);
+            }
         }), new TimerParam(data._EffectTime, delegate
         {
             
