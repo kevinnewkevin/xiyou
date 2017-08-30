a4a636ab9a9a4f8314a3521d3de616e4a3f787d9
W H <23@xinyueshikong.com>
2017-08-30 08:54:54 +0000

    lua   添加获取人物最大血    法   最小血法   的方法

--- trunk/config/scripts/common.lua
+++ trunk/config/scripts/common.lua
@@ -19,3 +19,80 @@ function percent()
 	return math.random(1, 100)
 
 end
+
+--获取人物属性值(property)最大的一个人物
+function max_property_one(battleid,unitid,property)
+
+	local t = Player.GetTargets(battleid,unitid)
+	
+	
+	local arr_property = {}    --人物属性
+	
+	
+	local arr_pro = {}   -- 人物
+	
+	
+	for i,v in ipairs(t) do
+	
+		local dangqian_property = Player.GetUnitProperty(battleid,v,property) --获取当前属性质量
+		
+		arr_property[i] = dangqian_property
+		
+		arr_pro[i] = v
+		
+	end
+	
+	local maxOfT = math.max(unpack(arr_property)) --最大数性值
+	
+	
+	local idx = 0
+	
+	for i=1,#arr_property,1 do
+	
+		if arr_property[i] == maxOfT then
+			idx = i
+		end
+		
+	end
+	
+	return arr_pro[idx]   --返回最大血的人物  单个
+	
+end
+
+
+--获取人物属性值(property)最少的一个人
+function min_property_one(battleid,unitid,property)
+
+	local t = Player.GetTargets(battleid,unitid)
+	
+	local arr_property = {}    --人物属性
+	
+	local arr_pro = {}   -- 人物
+	
+	for i,v in ipairs(t) do
+	
+		local dangqian_property = Player.GetUnitProperty(battleid,v,property) --获取当前属性质量
+		
+		arr_property[i] = dangqian_property
+		
+		arr_pro[i] = v
+		
+	end
+	
+	
+	local minOfT = math.min(unpack(arr_property)) -- 最小属性值
+	
+	
+	local idx = 0
+	
+	for i=1,#arr_property,1 do
+	
+		if arr_property[i] == minOfT then
+			idx = i
+		end
+		
+	end
+	
+	return arr_pro[idx]   --返回最大血的人物  单个
+	
+end
