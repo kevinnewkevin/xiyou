776839c2b4ca58a6b7b4826048d969495f6f23d6
Liu Zhaonan <3@xinyueshikong.com>
2017-09-15 07:23:02 +0000

    客户端逻辑

--- trunk/Client/Assets/Resources/ToLua/Lua/jiesuanjiemian.lua
+++ trunk/Client/Assets/Resources/ToLua/Lua/jiesuanjiemian.lua
@@ -27,6 +27,8 @@ local expBar;
 local starEff = nil;
 local starIdx = 0;
 
+local pvpHideItem = {};
+
 function jiesuanjiemian:OnEntry()
 	UIManager.RegIDirty("zhujiemian");
 	Define.LaunchUIBundle("icon");
@@ -41,6 +43,7 @@ end
 function jiesuanjiemian:OnInit()
 	self.contentPane = UIPackage.CreateObject("jiesuanjiemian", "jiesuanjiemian_com").asCom;
 	self:Center();
+	self.modal = true;
 
 	resultImg = self.contentPane:GetChild("n36").asLoader;
 
@@ -68,10 +71,76 @@ function jiesuanjiemian:OnInit()
 	exp = self.contentPane:GetChild("n18");
 	expBar = self.contentPane:GetChild("n17").asProgress;
 
+	pvpHideItem.a = self.contentPane:GetChild("n7");
+	pvpHideItem.b = self.contentPane:GetChild("n2");
+	pvpHideItem.c = self.contentPane:GetChild("n4");
+	pvpHideItem.d = self.contentPane:GetChild("n5");
+	pvpHideItem.e = self.contentPane:GetChild("n6");
+	pvpHideItem.f = self.contentPane:GetChild("n12");
+	pvpHideItem.g = self.contentPane:GetChild("n14");
+	pvpHideItem.h = self.contentPane:GetChild("n15");
+	pvpHideItem.i = self.contentPane:GetChild("n16");
+	pvpHideItem.j = self.contentPane:GetChild("n17");
+	pvpHideItem.k = self.contentPane:GetChild("n18");
+	pvpHideItem.l = self.contentPane:GetChild("n20");
+	pvpHideItem.m = self.contentPane:GetChild("n21");
+	pvpHideItem.n = self.contentPane:GetChild("n22");
+	pvpHideItem.o = self.contentPane:GetChild("n23");
+	pvpHideItem.p = self.contentPane:GetChild("n27");
+	pvpHideItem.q = self.contentPane:GetChild("n28");
+	pvpHideItem.r = self.contentPane:GetChild("n29");
+	pvpHideItem.s = self.contentPane:GetChild("n30");
+	pvpHideItem.t = self.contentPane:GetChild("n32");
+
 	jiesuanjiemian_FlushData();
 end
 
 function jiesuanjiemian_FlushData()
+
+	if Battle._BattleId == 0 then
+		pvpHideItem.a.visible = false;
+		pvpHideItem.b.visible = false;
+		pvpHideItem.c.visible = false;
+		pvpHideItem.d.visible = false;
+		pvpHideItem.e.visible = false;
+		pvpHideItem.f.visible = false;
+		pvpHideItem.g.visible = false;
+		pvpHideItem.h.visible = false;
+		pvpHideItem.i.visible = false;
+		pvpHideItem.j.visible = false;
+		pvpHideItem.k.visible = false;
+		pvpHideItem.l.visible = false;
+		pvpHideItem.m.visible = false;
+		pvpHideItem.n.visible = false;
+		pvpHideItem.o.visible = false;
+		pvpHideItem.p.visible = false;
+		pvpHideItem.q.visible = false;
+		pvpHideItem.r.visible = false;
+		pvpHideItem.s.visible = false;
+		pvpHideItem.t.visible = false;
+	else
+		pvpHideItem.a.visible = true;
+		pvpHideItem.b.visible = true;
+		pvpHideItem.c.visible = true;
+		pvpHideItem.d.visible = true;
+		pvpHideItem.e.visible = true;
+		pvpHideItem.f.visible = true;
+		pvpHideItem.g.visible = true;
+		pvpHideItem.h.visible = true;
+		pvpHideItem.i.visible = true;
+		pvpHideItem.j.visible = true;
+		pvpHideItem.k.visible = true;
+		pvpHideItem.l.visible = true;
+		pvpHideItem.m.visible = true;
+		pvpHideItem.n.visible = true;
+		pvpHideItem.o.visible = true;
+		pvpHideItem.p.visible = true;
+		pvpHideItem.q.visible = true;
+		pvpHideItem.r.visible = true;
+		pvpHideItem.s.visible = true;
+		pvpHideItem.t.visible = true;
+	end
+
 	star1_des.text = "击杀";
 	star2_des.text = "回合内胜利";
 	star3_des.text = "阵亡卡牌不超过";
