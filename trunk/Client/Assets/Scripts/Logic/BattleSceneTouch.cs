8fb014b2fca07556ad7b5f5f4371373ced15d71d
Liu Zhaonan <3@xinyueshikong.com>
2017-09-22 04:30:10 +0000

    客户端逻辑

--- trunk/Client/Assets/Scripts/Logic/BattleSceneTouch.cs
+++ trunk/Client/Assets/Scripts/Logic/BattleSceneTouch.cs
@@ -32,42 +32,39 @@ public class BattleSceneTouch : MonoBehaviour {
 
     void OnTouchMove(EventContext context)
     {
-        if (!Stage.isTouchOnUI && !Battle._BattleCamera._IsPlaying && (Battle.op != null && !Battle.op._IsPlaying))
+        if (_IsPress)
         {
-            if (_IsPress)
+            Vector2 pos = ((InputEvent)context.data).position;
+            Vector2 hPos = new Vector2(pos.x, 0f);
+            Vector2 vPos = new Vector2(0f, pos.y);
+            float crtHmag = hPos.magnitude;
+            float crtVmag = vPos.magnitude;
+            bool hanti = false;
+            if (_PreHmag > crtHmag)
+                hanti = true;
+
+            bool vanti = false;
+            if (_PreVmag > crtVmag)
+                vanti = true;
+
+            float h = Mathf.Abs(_PreHmag - crtHmag);
+            float v = Mathf.Abs(_PreVmag - crtVmag);
+            if (h > v)
+                Camera.main.transform.RotateAround(Battle._Center, Camera.main.transform.up, crtVmag * Time.deltaTime * 0.1f * (hanti ? -1f : 1f));
+            else if (h < v)
             {
-                Vector2 pos = ((InputEvent)context.data).position;
-                Vector2 hPos = new Vector2(pos.x, 0f);
-                Vector2 vPos = new Vector2(0f, pos.y);
-                float crtHmag = hPos.magnitude;
-                float crtVmag = vPos.magnitude;
-                bool hanti = false;
-                if (_PreHmag > crtHmag)
-                    hanti = true;
-
-                bool vanti = false;
-                if (_PreVmag > crtVmag)
-                    vanti = true;
-
-                float h = Mathf.Abs(_PreHmag - crtHmag);
-                float v = Mathf.Abs(_PreVmag - crtVmag);
-                if (h > v)
-                    Camera.main.transform.RotateAround(Battle._Center, Camera.main.transform.up, crtVmag * Time.deltaTime * 0.1f * (hanti ? -1f : 1f));
-                else if (h < v)
-                {
-                    float gap = Camera.main.transform.position.y - Battle._Center.y;
-                    if(gap >= 6f && vanti)
-                        Camera.main.transform.RotateAround(Battle._Center, Camera.main.transform.right, crtHmag * Time.deltaTime * 0.05f * (vanti ? -1f : 1f));
-                    else if(gap <= 2f && !vanti)
-                        Camera.main.transform.RotateAround(Battle._Center, Camera.main.transform.right, crtHmag * Time.deltaTime * 0.05f * (vanti ? -1f : 1f));
-                    else if(gap < 6f && gap > 2f)
-                        Camera.main.transform.RotateAround(Battle._Center, Camera.main.transform.right, crtHmag * Time.deltaTime * 0.05f * (vanti ? -1f : 1f));
-                }
-                Camera.main.transform.LookAt(Battle._Center);
-
-                _PreHmag = crtHmag;
-                _PreVmag = crtVmag;
+                float gap = Camera.main.transform.position.y - Battle._Center.y;
+                if(gap >= 6f && vanti)
+                    Camera.main.transform.RotateAround(Battle._Center, Camera.main.transform.right, crtHmag * Time.deltaTime * 0.05f * (vanti ? -1f : 1f));
+                else if(gap <= 2f && !vanti)
+                    Camera.main.transform.RotateAround(Battle._Center, Camera.main.transform.right, crtHmag * Time.deltaTime * 0.05f * (vanti ? -1f : 1f));
+                else if(gap < 6f && gap > 2f)
+                    Camera.main.transform.RotateAround(Battle._Center, Camera.main.transform.right, crtHmag * Time.deltaTime * 0.05f * (vanti ? -1f : 1f));
             }
+            Camera.main.transform.LookAt(Battle._Center);
+
+            _PreHmag = crtHmag;
+            _PreVmag = crtVmag;
         }
 
         if (DragDrop.inst.dragging)
