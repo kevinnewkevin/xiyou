102ef6ec4d6e8dfb6b6747e4fd08bdb4efb75aa2
Liu Wei <10@xinyueshikong.com>
2017-09-27 08:56:33 +0000

    商城

---
+++ trunk/Client/Assets/Resources/ToLua/Lua/cangbaoge.lua
@@ -0,0 +1,114 @@
+require "FairyGUI"
+
+cangbaoge = fgui.window_class(WindowBase)
+local Window;
+
+
+local cardGroupList;
+local crtGroupIdx = 0;
+local crtCardInstID = 0;
+local crtCardsFee = 0;
+local crtCardName;
+local isInGroup;
+local gold;
+local chopper;
+local stamaPoint;
+function cangbaoge:OnEntry()
+	Define.LaunchUIBundle("icon");
+	Window = cangbaoge.New();
+	Window:Show();
+end
+
+function cangbaoge:GetWindow()
+	return Window;
+end
+
+function cangbaoge:OnInit()
+	self.contentPane = UIPackage.CreateObject("cangbaoge", "cangbaoge_com").asCom;
+	self:Center();
+
+	self.closeButton = self.contentPane:GetChild("n2").asButton;
+	local shopList = self.contentPane:GetChild("n5").asList;
+	local feeMax = shopList.numItems;
+	local feeItem;
+	feeItem = shopList:GetChildAt(0);
+
+
+
+	local shopBtn0 = feeItem:GetChild("n9");
+	local itemLab0 = feeItem:GetChild("n6");
+	local moenyLab0 = shopBtn0 :GetChild("n3");
+	shopBtn0.data = 1000;
+	shopBtn0.onClick:Add(cangbaoge_OnBuyClick);
+	local shopData = ShopData.GetData(1000);
+	itemLab0.text = shopData._Name;
+	moenyLab0.text = shopData._Price;
+
+	local shopBtn1 = feeItem:GetChild("n11");
+	local itemLab1 = feeItem:GetChild("n7");
+	local moenyLab1 = shopBtn1 :GetChild("n3");
+	shopBtn1.data = 1001;
+	shopBtn1.onClick:Add(cangbaoge_OnBuyClick);
+	local shopData = ShopData.GetData(1001);
+	itemLab1.text = shopData._Name;
+	moenyLab1.text = shopData._Price;
+
+	local shopBtn2 = feeItem:GetChild("n12");
+	local itemLab2 = feeItem:GetChild("n8");
+	local moenyLab2 = shopBtn2 :GetChild("n3");
+	shopBtn2.data = 1002;
+	shopBtn2.onClick:Add(cangbaoge_OnBuyClick);
+	local shopData = ShopData.GetData(1002);
+	itemLab2.text = shopData._Name;
+	moenyLab2.text = shopData._Price;
+
+	local moneyGroup = self.contentPane:GetChild("n0").asCom;
+	gold = moneyGroup:GetChild("n5");
+	chopper = moneyGroup:GetChild("n7");
+	stamaPoint = moneyGroup:GetChild("n12");
+	cangbaoge_FlushData();
+end
+
+
+
+
+function cangbaoge:OnUpdate() 
+	if UIManager.IsDirty("cangbaoge") then
+		cangbaoge_FlushData();
+		UIManager.ClearDirty("cangbaoge");
+	end
+end
+
+function cangbaoge:OnTick()
+	
+end
+
+function cangbaoge:isShow()
+	return Window.isShowing;
+end
+
+function cangbaoge:OnDispose()
+	Window:Dispose();
+end
+
+function cangbaoge:OnHide()
+	Window:Hide();
+end
+
+function cangbaoge_FlushData()
+	gold.text = GamePlayer._Data.IProperties[8];
+	chopper.text = GamePlayer._Data.IProperties[6];
+	stamaPoint.text = GamePlayer._Data.IProperties[10];
+
+end
+
+function cangbaoge_OnBuyClick(context)
+
+	local shopData = ShopData.GetData(1002);
+	if shopData._Price > GamePlayer._Data.IProperties[8] then
+		local MessageBox = UIManager.ShowMessageBox();
+		MessageBox:SetData("提示", "金不够", true);
+		return;
+	end
+	Proxy4Lua.BuyShopItem(context.sender.data);
+end
---
+++ trunk/Client/Assets/Resources/ToLua/Lua/kaikabao.lua
@@ -0,0 +1,175 @@
+require "FairyGUI"
+
+kaikabao = fgui.window_class(WindowBase)
+local Window;
+
+local hitNextLbl;
+local hitNextBtn;
+
+local dropList;
+local openTrans;
+local outTrans;
+
+local openCom;
+local outCom;
+
+local dropItemUrl = "url://kaikabao/daoju_com";
+
+local readyOutDrop;
+local canExit;
+
+local timeOut;
+local boxEff;
+local boxEffHolder;
+
+function kaikabao:OnEntry()
+	Define.LaunchUIBundle("icon");
+	Window = kaikabao.New();
+	Window:Show();
+end
+
+function kaikabao:GetWindow()
+	return Window;
+end
+
+function kaikabao:OnInit()
+	self.contentPane = UIPackage.CreateObject("kaikabao", "kaikabao_com").asCom;
+	self:Center();
+
+	hitNextLbl = self.contentPane:GetChild("n3").asTextField;
+	hitNextBtn = self.contentPane:GetChild("n4");
+	hitNextBtn.onClick:Add(kaikabao_OnExit);
+	hitNextBtn.enabled = false;
+	hitNextLbl.visible = false;
+
+	dropList = self.contentPane:GetChild("n2").asList;
+	dropList.visible = false;
+	openCom = self.contentPane:GetChild("n0").asCom;
+	outCom = self.contentPane:GetChild("n5").asCom;
+	outCom.visible = false;
+	boxEffHolder = outCom:GetChild("n6").asGraph;
+	openTrans = self.contentPane:GetTransition("t1");
+	outTrans = outCom:GetTransition("t0");
+
+	openCom.onClick:Add(kaikabao_OnBoxBtn);
+
+
+
+	timeOut = {};
+	timeOut.max = 2;
+	timeOut.count = 0;
+
+	kaikabao_FlushData();
+end
+
+function kaikabao_OnBoxBtn(context)
+	timeOut = nil;
+	outCom.visible = true;
+	openCom.visible = false;
+	outTrans:Play();
+
+	readyOutDrop = {};
+	readyOutDrop.max = 12;
+	readyOutDrop.count = 0;
+
+	boxEff = "effect/baoxiangguangmang";
+	boxEffHolder:SetNativeObject(Proxy4Lua.GetAssetGameObject(boxEff));
+end
+
+function kaikabao_OnExit(context)
+
+	Window:Hide();			
+	
+	Proxy4Lua.UnloadAsset(boxEff);
+	boxEff = "";
+	boxEffHolder:SetNativeObject(Proxy4Lua.GetAssetGameObject(""));
+end
+
+function kaikabao_FlushData()
+	local dropItem;
+	local dropItemCount;
+	local itemInst;
+	local stack;
+	local name;
+	local quality;
+	local icon;
+	local iData;
+
+	local showChapters = UIParamHolder.Get("showChaptersDrop");
+
+		dropItemCount = ShopSystem.BuyItems.Length;
+		dropList:RemoveChildrenToPool();
+		for i=1, dropItemCount do
+			itemInst = ShopSystem.BuyItems[i-1];
+			dropItem = dropList:AddItemFromPool(dropItemUrl);
+			if itemInst ~= nil then
+				iData = ItemData.GetData(itemInst.ItemId);
+				quality = dropItem:GetChild("n0").asLoader;
+				icon = dropItem:GetChild("n1").asLoader;
+				stack = dropItem:GetChild("n2").asTextField;
+				--name = dropItem:GetChild("n3").asTextField;
+
+				stack.text = itemInst.Stack;
+				if iData ~= nil then
+					--name.text = iData._Name;
+					icon.url = "ui://" .. iData._Icon;
+					quality.url = "ui://" .. iData._IconBack;
+				end
+			end
+		end
+	end
+
+function kaikabao:OnUpdate()
+	if UIManager.IsDirty("kaikabao") then
+		kaikabao_FlushData();
+		UIManager.ClearDirty("kaikabao");
+	end
+
+	if readyOutDrop ~= nil then
+		readyOutDrop.count = readyOutDrop.count + 1;
+		if readyOutDrop.count >= readyOutDrop.max then
+			dropList.visible = true;
+			openTrans:Play();
+			readyOutDrop = nil;
+			canExit = {};
+			canExit.max = 20;
+			canExit.count = 0;
+		end
+	end
+
+	if canExit ~= nil then
+		canExit.count = canExit.count + 1;
+		if canExit.count >= canExit.max then
+			hitNextLbl.visible = true;
+			hitNextBtn.enabled = true;
+			canExit = nil;
+		end
+	end
+end
+
+function kaikabao:OnTick()
+	if timeOut ~= nil then
+		timeOut.count = timeOut.count + 1;
+		if timeOut.count >= timeOut.max then
+			kaikabao_OnBoxBtn();
+			timeOut = nil;
+		end
+	end
+end
+
+function kaikabao:isShow()
+	return Window.isShowing;
+end
+
+function kaikabao:OnDispose()
+	Window:Dispose();
+end
+
+function kaikabao:OnHide()
+	openCom.visible = true;
+	hitNextBtn.enabled = false;
+	hitNextLbl.visible = false;
+	dropList.visible = false;
+	outCom.visible = false;
+	Window:Hide();
+end
