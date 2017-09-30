94609b3dc9d32fa946b698f33aaeb73679cdf26a
Guo Yanlin <hotlala8088@outlook.com>
2017-09-30 04:17:22 +0000

    rpc 提交

---
+++ trunk/server/src/logic/prpc/ShopType.go
@@ -0,0 +1,42 @@
+package prpc
+// enum ShopType
+const( 
+  SHT_Min = 0
+  SHT_Card = 1
+  SHT_BuyCopper = 2
+  SHT_Max = 3
+)
+const( 
+  K_SHT_Min = "SHT_Min"
+  K_SHT_Card = "SHT_Card"
+  K_SHT_BuyCopper = "SHT_BuyCopper"
+  K_SHT_Max = "SHT_Max"
+)
+func ToName_ShopType( id int )string{
+  switch(id){
+    case SHT_Min:
+      return "SHT_Min"
+    case SHT_Card:
+      return "SHT_Card"
+    case SHT_BuyCopper:
+      return "SHT_BuyCopper"
+    case SHT_Max:
+      return "SHT_Max"
+    default:
+      return ""
+  }
+}
+func ToId_ShopType( name string ) int {
+  switch(name){
+    case "SHT_Min":
+      return SHT_Min
+    case "SHT_Card":
+      return SHT_Card
+    case "SHT_BuyCopper":
+      return SHT_BuyCopper
+    case "SHT_Max":
+      return SHT_Max
+    default:
+      return -1
+  }
+}
