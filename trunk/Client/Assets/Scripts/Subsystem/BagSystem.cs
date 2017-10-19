c85fb7ad5d7b0be10676116ccd7f952c4c04efd0
Liu Zhaonan <3@xinyueshikong.com>
2017-10-19 10:37:49 +0000

    客户端逻辑

--- trunk/Client/Assets/Scripts/Subsystem/BagSystem.cs
+++ trunk/Client/Assets/Scripts/Subsystem/BagSystem.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class BagSystem {
 
-    public static Dictionary<int, List<COM_ItemInst>> _ItemsByType;
+    public static Dictionary<int, List<COM_ItemInst>> _ItemsByType = new Dictionary<int, List<COM_ItemInst>>();
 
 	public static void Init(COM_ItemInst[] items)
     {
-		_ItemsByType = new Dictionary<int, List<COM_ItemInst>>();
 		List<COM_ItemInst> itemList = new List<COM_ItemInst> ();
 
         if ( items != null )
