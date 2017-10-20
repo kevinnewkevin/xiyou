61a89a53d8f821fb5720f1666a9acfe9efe02735
W H <9@xinyueshikong.com>
2017-10-20 09:20:19 +0000

    DB配置

--- trunk/config/scripts/env.lua
+++ trunk/config/scripts/env.lua
@@ -1,3 +1,3 @@
-Env.setEnvString("V_MySqlData",		"xysk:123456@tcp(10.10.10.254:3306)/xygame");
-
+--Env.setEnvString("V_MySqlData",		"xysk:123456@tcp(10.10.10.254:3306)/xygame");
+Env.setEnvString("V_MySqlData",		"xysk:123456@tcp(106.75.8.248:3306)/xygame");
 Env.setEnvInt("V_GM", 1);
