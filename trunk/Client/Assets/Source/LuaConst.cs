072de3e9e9ec388a37a12209a9117b3cc563c52c
Liu Zhaonan <3@xinyueshikong.com>
2017-10-24 03:10:14 +0000

    客户端逻辑

--- trunk/Client/Assets/Source/LuaConst.cs
+++ trunk/Client/Assets/Source/LuaConst.cs
@@ -15,7 +15,7 @@ public static class LuaConst
     public static string osDir = "";        
 #endif
 
-    public static string luaResDir = string.Format("{0}/{1}/Lua", Application.streamingAssetsPath, Define.PackageVersion);      //手机运行时lua文件下载目录    
+    public static string luaResDir = string.Format("{0}/{1}/Lua/", Application.streamingAssetsPath, Define.PackageVersion);      //手机运行时lua文件下载目录    
 
 #if UNITY_EDITOR_WIN || NITY_STANDALONE_WIN    
     public static string zbsDir = "D:/ZeroBraneStudio/lualibs/mobdebug";        //ZeroBraneStudio目录       
