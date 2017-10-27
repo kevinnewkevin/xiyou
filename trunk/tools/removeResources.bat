a1a9079021b81de3c11a7f0271fac5ba6def5f6f
Liu Zhaonan <3@xinyueshikong.com>
2017-10-27 09:23:40 +0000

    客户端逻辑

--- trunk/tools/removeResources.bat
+++ trunk/tools/removeResources.bat
@@ -2,7 +2,6 @@ rd/s/q ..\Client\Assets\Resources\Fonts\
 rd/s/q ..\Client\Assets\Resources\Anim\
 rd/s/q ..\Client\Assets\Resources\Audio\
 rd/s/q ..\Client\Assets\Resources\Effect\
-rd/s/q ..\Client\Assets\Resources\Icon\
 rd/s/q ..\Client\Assets\Resources\Player\
 rd/s/q ..\Client\Assets\Resources\Table\
 rd/s/q ..\Client\Assets\Resources\ToLua\Lua\
