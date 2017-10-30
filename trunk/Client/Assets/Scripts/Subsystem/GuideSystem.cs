e916fa2da02ee17b0e8afdfed37afbb1d833a463
Liu Zhaonan <3@xinyueshikong.com>
2017-10-30 06:14:01 +0000

    客户端逻辑

---
+++ trunk/Client/Assets/Scripts/Subsystem/GuideSystem.cs
@@ -0,0 +1,29 @@
+﻿using FairyGUI;
+
+public class GuideSystem  {
+
+    static public void Init()
+    {
+        
+    }
+
+    static public void OpenUI(string ui, Window win)
+    {
+        LuaManager.Call("global.lua", "WhenUIOpen", ui, win);
+    }
+
+    static public void StartGuide(GObject aim)
+    {
+        
+    }
+
+    static public bool IsNotFinish(int idx)
+    {
+        return true;
+    }
+
+    static public void SetFinish(int idx)
+    {
+        
+    }
+}
