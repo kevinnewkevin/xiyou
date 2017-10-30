74f3e1db715cfca06c60297d79b2c7f0d4c2c463
Liu Zhaonan <3@xinyueshikong.com>
2017-10-30 08:08:54 +0000

    客户端逻辑

--- trunk/config/scripts/guide.lua
+++ trunk/config/scripts/guide.lua
@@ -1,22 +1,22 @@
 
 -- UI打开时调用
 function WhenUIOpen(uiName, uiWindow)
-	if uiName == "denglu" then
+	--[[if uiName == "denglu" then
 		if GuideSystem.IsNotFinish(1) then
 			GuideSystem.StartGuide(uiWindow.contentPane:GetChild("n10"):GetChild("n11"));
 			GuideSystem.SetFinish(1);
 		end
-	end
+	end--]]
 end
 
 -- 特殊事件调用
 function SpecialEvent(type, param1)
-	if type == "battlestart" then
+	--[[if type == "battlestart" then
 		if Battle._Turn == 1 then
 			if GuideSystem.IsNotFinish(2) then
 				GuideSystem.StartGuide();
 				GuideSystem.SetFinish(2);
 			end
 		end
-	end
+	end--]]
 end
