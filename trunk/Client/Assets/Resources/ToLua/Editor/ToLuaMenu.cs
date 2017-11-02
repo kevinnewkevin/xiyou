2800fc21279089ba51c55b33a422a388b69af33f
Liu Zhaonan <3@xinyueshikong.com>
2017-11-02 07:03:11 +0000

    客户端逻辑

--- trunk/Client/Assets/Resources/ToLua/Editor/ToLuaMenu.cs
+++ trunk/Client/Assets/Resources/ToLua/Editor/ToLuaMenu.cs
@@ -106,15 +106,15 @@ public static class ToLuaMenu
 
         if (files.Length < 3 && beCheck)
         {
-            if (EditorUtility.DisplayDialog("自动生成", "点击确定自动生成常用类型注册文件， 也可通过菜单逐步完成此功能", "确定", "取消"))
-            {
+//            if (EditorUtility.DisplayDialog("自动生成", "点击确定自动生成常用类型注册文件， 也可通过菜单逐步完成此功能", "确定", "取消"))
+//            {
                 beAutoGen = true;
                 GenLuaDelegates();
                 AssetDatabase.Refresh();
                 GenerateClassWraps();
                 GenLuaBinder();
                 beAutoGen = false;                
-            }
+//            }
 
             beCheck = false;
         }
