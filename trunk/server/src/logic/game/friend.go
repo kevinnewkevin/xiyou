7f7ff72aef61daba9f16e86b296d0238d94a0875
W H <25@xinyueshikong.com>
2017-11-08 09:17:55 +0000

    增加instid

---
+++ trunk/server/src/logic/game/friend.go
@@ -0,0 +1,171 @@
+package game
+
+import (
+	"logic/log"
+	"logic/prpc"
+	"strconv"
+)
+
+////////////////////////////////////////////////////
+////好友
+////////////////////////////////////////////////////
+
+func (this *GamePlayer) SerchFriendByName(name string) {
+	t := FindPlayerByInstName(name)
+
+	if t == nil {
+		log.Println("不存在")
+		return
+	}
+
+	if t.session == nil {
+		log.Println("不在线")
+		return
+	}
+
+	for _, b := range t.Enemys {
+		if b.Name == name{
+			log.Println("你在对方黑名单中,不能查看")
+			return
+		}
+	}
+
+	info := prpc.COM_Friend{}
+	info.InstId = t.MyUnit.InstId
+	info.Name = t.MyUnit.InstName
+	info.Level = t.MyUnit.Level
+	info.DisplayID = t.MyUnit.UnitId
+
+	t.session.FriendInfo([]prpc.COM_Friend{info})		//向对方发送好友信息
+
+}
+
+func (this *GamePlayer) SerchFriendRandom() {
+	var step int32 = 10
+	all := []prpc.COM_Friend{}
+
+	low := this.MyUnit.Level - step
+	if low <= 1{
+		low = 1
+	}
+
+	high := this.MyUnit.Level - step
+
+	for _, p := range PlayerStore {
+		if low <= p.MyUnit.Level && p.MyUnit.Level <= high{
+			info := prpc.COM_Friend{}
+			info.InstId = p.MyUnit.InstId
+			info.Name = p.MyUnit.InstName
+			info.Level = p.MyUnit.Level
+			info.DisplayID = p.MyUnit.UnitId
+
+			all = append(all, info)
+		}
+	}
+
+	if len(all) == 0 {
+		for i := 1; i <= 6; i++ {
+			info := prpc.COM_Friend{}
+			info.InstId = int64(i)
+			info.Name = strconv.Itoa(i)
+			info.Level = this.MyUnit.Level
+			info.DisplayID = this.MyUnit.UnitId
+
+			all = append(all, info)
+		}
+	}
+
+	if len(all) > 6 {
+		this.session.FriendInfo(all[:6])
+	} else {
+		this.session.FriendInfo(all)
+	}
+
+}
+
+func (this *GamePlayer) ApplicationFriend(name string) {
+	t := FindPlayerByInstName(name)
+
+	if t == nil {
+		log.Println("不存在")
+		return
+	}
+
+	if t.session == nil {
+		log.Println("不在线")
+		return
+	}
+
+	for _, b := range t.Enemys {
+		if b.Name == name{
+			log.Println("你在对方黑名单中,不能添加好友")
+			return
+		}
+	}
+
+	t.session.ApplyFriend(this.MyUnit.InstName)		//向对方发送好友信息
+}
+
+func (this *GamePlayer) ProcessingFriend(name string) {
+	t := FindPlayerByInstName(name)
+
+	if t == nil {
+		log.Println("不存在")
+		return
+	}
+
+	if t.session == nil {
+		log.Println("不在线")
+		return
+	}
+
+	for _, b := range t.Enemys {
+		if b.Name == name{
+			log.Println("你在对方黑名单中,不能添加好友")
+			return
+		}
+	}
+
+	//把自己加入到对方好友名单
+	info := prpc.COM_Friend{}
+	info.InstId = this.MyUnit.InstId
+	info.Name = this.MyUnit.InstName
+	info.Level = this.MyUnit.Level
+	info.DisplayID = this.MyUnit.UnitId
+	t.Friends = append(t.Friends, &info)
+	t.session.RecvFriend(info)
+
+	//把别人加入到自己的好友名单
+	t_info := prpc.COM_Friend{}
+	info.InstId = t.MyUnit.InstId
+	t_info.Name = t.MyUnit.InstName
+	t_info.Level = t.MyUnit.Level
+	t_info.DisplayID = t.MyUnit.UnitId
+	this.Friends =  append(this.Friends, &t_info)
+	this.session.RecvFriend(t_info)
+
+}
+
+func (this *GamePlayer) DeleteFriend() {
+
+}
+
+////////////////////////////////////////////////////
+////黑名单
+////////////////////////////////////////////////////
+
+func (this *GamePlayer) AddBlackList() {
+
+}
+
+func (this *GamePlayer) DeleteBlackList() {
+
+}
+
+////////////////////////////////////////////////////
+////信息
+////////////////////////////////////////////////////
+
+func (this *GamePlayer) SendMessage() {
+
+}
