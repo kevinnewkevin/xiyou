c40a61bf08b7d08b65f77eab551151f67f3798bb
Liu Zhaonan <3@xinyueshikong.com>
2017-11-16 09:24:59 +0000

    战队

---
+++ trunk/Client/Assets/Resources/ToLua/Lua/squadSetting.lua
@@ -0,0 +1,167 @@
+require "FairyGUI"
+
+squadSetting = fgui.window_class(WindowBase)
+local Window;
+
+local typeList;
+local setCom;
+local requestCom;
+local crtType;
+
+local guildName;
+local guildId;
+local guildCount;
+local guildScore;
+local guildGiftWeek;
+local guildNeedCheck;
+local guildNeededLoader;
+local guildNeededBtn;
+
+local requestList;
+
+function squadSetting:OnEntry()
+	Window = squadSetting.New();
+	Window:Show();
+end
+
+function squadSetting:GetWindow()
+	return Window;
+end
+
+function squadSetting:OnInit()
+	self.contentPane = UIPackage.CreateObject("bangpai", "bangpaishezhi_com").asCom;
+	self:Center();
+	self.closeButton = self.contentPane:GetChild("n37");
+
+	typeList = self.contentPane:GetChild("n4");
+	setCom = self.contentPane:GetChild("n5");
+	requestCom = self.contentPane:GetChild("n39");
+	typeList.onClickItem:Add(squadSetting_OnTypeSelect);
+
+	guildName = setCom:GetChild("n12");
+	guildId = setCom:GetChild("n13");
+	guildCount = setCom:GetChild("n14");
+	guildScore = setCom:GetChild("n15");
+	guildGiftWeek = setCom:GetChild("n16");
+	guildNeedCheck = setCom:GetChild("n21");
+	guildNeededLoader = setCom:GetChild("n33").asLoader;
+	guildNeededBtn = setCom:GetChild("n22");
+	guildNeededBtn.OnClick:Add(squadSetting_OnNeededChange);
+
+	requestList = requestCom:GetChild("n39").asList;
+	requestList:SetVirtual();
+	requestList.itemRenderer = squadSetting_RenderListItem;
+
+	crtType = 0;
+	typeList.selectedIndex = crtType;
+
+	squadSetting_FlushData();
+end
+
+function squadSetting_OnTypeSelect()
+	crtType = typeList.selectedIndex;
+	UIManager.SetDirty("squadSetting");
+end
+
+function squadSetting:OnUpdate()
+	if UIManager.IsDirty("squadSetting") then
+		squadSetting_FlushData();
+		UIManager.ClearDirty("squadSetting");
+	end
+end
+
+function squadSetting:OnTick()
+	
+end
+
+function squadSetting:isShow()
+	return Window.isShowing;
+end
+
+function squadSetting:OnDispose()
+	Window:Dispose();
+end
+
+function squadSetting:OnHide()
+	Window:Hide();
+end
+
+function squadSetting_OnNeededChange()
+	
+end
+
+function squadSetting_RenderListItem(index, obj)
+	if obj == nil then
+		return;
+	end
+
+	local data = GuildSystem.myGuild.RequestList[index];
+	local playerName = obj:GetChild("n40");
+	local headCom = obj:GetChild("n39");
+	local headIcon = headCom:GetChild("n5").asLoader;
+	local lv = headCom:GetChild("n3").asTextField;
+	local dayBefore = obj:GetChild("n42");
+	local tiantiLv = obj:GetChild("n41").asLoader;
+	local add = obj:GetChild("n43").asButton;
+	local ignore = obj:GetChild("n44").asButton;
+	add.onClick:Add(squadSetting_OnAdd);
+	ignore.onClick:Add(squadSetting_OnIgnore);
+	obj.data = data.RoleId;
+
+	playerName.text = data.RoleName;
+	local eData = EntityData.GetData(data.UnitId);
+	local dData;
+	if eData ~= nil then
+		dData = DisplayData.GetData(eData._DisplayId);
+	end
+	if dData ~= nil then
+		headIcon.url = "ui://" .. dData._HeadIcon;
+	else
+		headIcon.url = "";
+	end
+	lv.text = data.Level;
+	dayBefore.text = "";
+	tiantiLv.url = "ui://bangpai/xiao_duanwei" .. GamePlayer.RankLevel(data.TianTiVal);
+end
+
+function squadSetting_OnAdd(context)
+	if context.sender.data == nil then
+		return;
+	end
+
+	Proxy4Lua.AcceptRequestGuild(context.sender.data);
+end
+
+function squadSetting_OnIgnore(context)
+	if context.sender.data == nil then
+		return;
+	end
+
+	Proxy4Lua.RefuseRequestGuild(context.sender.data);
+end
+	
+function squadSetting_FlushData()
+	requestList.numItems = GuildSystem.myGuild.RequestList.Lenght;
+
+	if crtType == 0 then
+		setCom.visible = true;
+		requestCom.visible = false;
+	else
+		setCom.visible = false;
+		requestCom.visible = true;
+	end
+	--TODO
+	guildName.text = "帮派名称: " .. GuildSystem.myGuild.GuildName;
+	guildId = "帮派编号: " .. GuildSystem.myGuild.GuildId;
+	guildCount = "帮派人数: " .. GuildSystem.myGuild.GuildName;
+	guildScore = "帮派积分: " .. GuildSystem.myGuild.GuildName;
+	guildGiftWeek = "每周捐献: " .. GuildSystem.myGuild.GuildName;
+	if GuildSystem.myGuild.IsRatify then
+		guildNeedCheck:GetChild("n1").visible = true;
+		guildNeedCheck:GetChild("n2").visible = false;
+	else
+		guildNeedCheck:GetChild("n1").visible = false;
+		guildNeedCheck:GetChild("n2").visible = true;
+	end
+	guildNeededLoader.url = "ui://bangpai/xiao_duanwei" .. GamePlayer.RankLevel(Require);
+end
