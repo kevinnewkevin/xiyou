d792fde5f6ae7bd8cc0dd8ddeda691f461f6a5b3
Liu Zhaonan <3@xinyueshikong.com>
2017-11-22 02:23:31 +0000

    战队

---
+++ trunk/Client/Assets/Scripts/ParticalScale.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParticalScale : MonoBehaviour
+{
+    public float scanleSize = 1f;
+    List<float> initialSize = new List<float>();
+
+    void Awake()
+    {
+        ParticleSystem[] particles = gameObject.GetComponentsInChildren<ParticleSystem>();
+
+        foreach(ParticleSystem particle in particles)
+        {
+            initialSize.Add(particle.startSize);
+            ParticleSystemRenderer renderer = particle.GetComponent<ParticleSystemRenderer>();
+            if (renderer)
+            {
+                initialSize.Add(renderer.lengthScale);
+                initialSize.Add(renderer.velocityScale);
+            }
+        }
+    }
+
+    void Start()
+    {
+        int arrayIndex = 0;
+        ParticleSystem[] particles = gameObject.GetComponentsInChildren<ParticleSystem>();
+        foreach(ParticleSystem particle in particles)
+        {
+            particle.startSize = initialSize [arrayIndex++] * scanleSize;
+            ParticleSystemRenderer renderer = particle.GetComponent<ParticleSystemRenderer>();
+            if (renderer)
+            {
+                renderer.lengthScale = initialSize[arrayIndex++] * scanleSize;
+                renderer.velocityScale = initialSize[arrayIndex++] * scanleSize;
+            }
+        }
+    }
+}
